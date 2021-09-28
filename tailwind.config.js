04c4a02f65fddf9ef2650e3ef31d09313d01703d
nickcarva <nicolas@ecovitasdigital.com>
2021-09-27 21:21:35 -0300

    🔧 CONFIG: global styles | fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +9,13 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl'), fontFamily: 'Poppins-Black' },
+        'h2': { fontSize: theme('fontSize.xl'), fontFamily: 'Poppins-ExtraBold' },
+        'h3': { fontSize: theme('fontSize.lg'), fontFamily: 'Poppins-Medium' },
+      })
+    })
+  ],
 }
